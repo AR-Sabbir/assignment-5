7f200a5fd05796fb809f239c278d9b59230a79de
AR-Sabbir <101645575+AR-Sabbir@users.noreply.github.com>
2022-04-13 11:25:07 -0700

    Delete class4 directory

--- class4/app.js
+++
@@ -1,8 +0,0 @@
-//    let name =prompt('name')
-//    let age =prompt('age')
-  
-   
-// ageCal(name,age)
-let amount = prompt('Amount');
-    let currency = prompt('Currency'); 
-    convert(amount,currency)
